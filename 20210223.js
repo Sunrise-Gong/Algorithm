f8d5dc78a28336ba2ad386fe23526833a08a7c43
Sunrise Gong <rhdgoehe2@gmail.com>
2021-02-24 11:57:53 +0900

    [Programmers] Sum between two integers

--- 20210223.js
+++ 20210223.js
@@ -60,14 +60,43 @@ function solution_2(participant, completion) {
 }
 
 function solution_3(participant, completion) {
-    
-    return participant.find(name=> { 
-        return !completion[name]-- 
-        // 이름에 대한 값이 없는 경우 undefind 가 나올 것이다.
-        // 있는 경우는 값에서 1을 빼는데
-        // 주의할 점은 1에서 1을 빼면 1이 리턴되고 값이 0으로 바뀐다.
-        // 그래서 그 다음에 0에서 1을 빼면 0이 리턴되므로 falsy한 값이 나온다.
-    },completion.map(name=>completion[name]=(completion[name]|0)+1))
+        /*
+        배열은 객체와 같이 키와 값을 추가할 수 있습니다. 이점을 생각해서
+        맵 메소드로 원본 배열을 객체의 역할도 겸하도록 만들어보겠습니다.
+        그러면 순회하지 않아도 되니까 복잡도가 감소할 것입니다.
+        
+        속성은 선수의 이름이고 그 이름을 가진 인원 수를 값으로 넣겠습니다.
+        동명이인이 없다면 -> 바보: 1  이런식으로 키값쌍이 배열에 추가 됩니다.
+        */
+        completion.map(name=>completion[name]=(completion[name]|0)+1)
+        /*
+        completion[name]|0 의 의미는 
+        completion[name]의 값이 있다면 거기에 +1
+        없다면 0에 +1
+        문제에서 말하는 동명이인이 있다면 그 인원수 만큼 더해집니다.
+        
+        위의 map 함수 부분이 
+        find 메소드의 두번째 인자로 들어간 식이 다른 분의 풀이 였으나
+        가독성을 위해서 분리를 해보았습니다.
+        (find 메소드의 두번째 인자로 들어가는 경우 첫번째 인자보다 먼저 실행됩니다.)
+        */
+        return participant.find(name=> {
+            return !completion[name]-- 
+        })
+        /*
+        find의 콜백함수는 객체처럼 완주명단을 조회해서 그 속성의 값을 1씩 빼는데
+        주의할 점은 리턴 값이 빼기전의 값이 나온다는 것입니다. 
+        
+        예를들어 1 - 1 을 했는데 리턴 값은 1 이 됩니다. 
+        하지만 그다음 조회시에는 0 - 1 이 되서 0 이 리턴 되죠.
+        
+        0은 falsy 한 값이니까 그 값에 not 연산자를 걸어서
+        find가 그 값(name)을 리턴 하도록 하는 것으로 마무리 됩니다.
+        
+        아예 완주자 명단에 있지도 않은 이름이라면 
+        undefined 가 not 연산자에 의해서 리턴되면서
+        find가 그 값(name)을 리턴 하도록 하는 것으로 마무리 됩니다.
+        */
 
     /*
     테스트 1 〉	통과 (0.12ms, 29.8MB)
