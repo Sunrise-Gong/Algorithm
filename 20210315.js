7ccf5822fc7a72bd1431f9826d25988d114a1a51
Sunrise Gong <rhdgoehe2@gmail.com>
2021-03-16 23:34:39 +0900

    [Programmers] carpet_1

--- 20210315.js
+++ 20210315.js
@@ -14,10 +14,20 @@ function solution(number, k) {
     return stack.slice(0, number.length-k).join('')
 }
 /*
-k 가 0 이상일 경우 ex) 9999(같은 숫자가 반복되는 경우 제거가 안됨)
+문자열 number를 순회하면서
+
+현재 요소가 배열 stack의 마지막 요소보다 크고 k가 0보다 크다면
+{stack의 마지막 요소를 제거하고 
+k를 1 감소 시킨다.}->이 과정을 계속 실행 
+
+현재 요소가 stack의 마지막 요소보다 작다면
+stack에 현재 요소를 추가한다.
+
+k 가 0 이상일 경우 ex) 9999(같은 숫자가 반복되는 경우 제거가 안되서 k가 0이 안됨)
 k 만큼 stack의 뒤의 요소를 없애서 리턴해야 한다.
 slice 를 이용할 수도 있고
 splice를 이용할 수도 있다.
+
 splice 사용예시
 stack = [1, 2, 3, 4, 5, 6, 7] 
 stack.splice(3,2) -> [4, 5]
