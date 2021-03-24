2a1f2b46333f79929fe2940da58de6ef2bcd7e92
Sunrise Gong <rhdgoehe2@gmail.com>
2021-03-24 17:16:38 +0900

    [Programmers] joystick_2

--- 20210323.js
+++ 20210323.js
@@ -17,42 +17,52 @@
 // 1번 인덱스부터 뭉쳐있는 A가 더 길면 좌로
 function solution(name) {
     var answer = 0;
-    const leftCycle = () => {
-        
+    const leftCycle = () => { // 두번 째 인덱스에 A가 있다면 제거하고 좌측 순회
+
     }
-    
-    const rightCycle = () => {
-        
+
+    const rightCycle = () => { // 마지막 인덱스에 A가 있다면 제거하고 우측 순회
+
     }
+    // 두번째 인덱스가 A 인 경우
     if (name[1] === 'A') { // (좌A)
-        if (name[name.length-1] === 'A') { // (좌A && 우A)
+        // 두번째 인덱스가 A 인 경우 & 마지막 인덱스가 A 인 경우
+        if (name[name.length - 1] === 'A') { // (좌A && 우A)
+            // 양쪽 A의 갯수를 구하고
             let lA
             let rA
-            if (lA > rA) { // 좌순회
+
+            // 좌측 A의 개수가 많으면 좌측 A를 제거하고 좌순회
+            if (lA > rA) {
                 leftCycle
-            } else { // 우순회
+                // 우측 A의 개수가 많으면 우측 A를 제거하고 우순회
+            } else {
                 rightCycle
             }
+
+            // 두번째 인덱스가 A 인 경우 && 마지막 인덱스 A가 아닌 경우    
         } else { // 좌순회 좌A
             leftCycle
         }
-    } else { // 우순회 (우A or 무A)
+
+        // 두번째 인덱스가 A가 아닌 경우 
+    } else {
         rightCycle
     }
     return answer;
 }
-
+//------------------------------------------------------------------------------
 function solution(name) {
     var answer = 0;
     const leftCycle = () => {
-        
+
     }
-    
+
     const rightCycle = () => {
-        
+
     }
     if (name[1] === 'A') { // (좌A)
-        if (name[name.length-1] === 'A') { // (좌A && 우A)
+        if (name[name.length - 1] === 'A') { // (좌A && 우A)
             let lA
             let rA
             lA > rA ? leftCycle : rightCycle
@@ -64,10 +74,45 @@ function solution(name) {
     }
     return answer;
 }
+
+//------------------------------------------------------------------------------다른 사람 풀이
+
+// 연속된 A의 개수를 구하는 함수
+function continuous(name) {
+    let repeat = 0;
+    for (let i = 0; i < name.length; i++) {
+        if (name[i] != "A") break;
+        repeat++;
+    }
+    return repeat;
+}
+
+// 위 또는 아래 어느쪽이 빠른지 판단해서 짧은 이동횟수를 반환하는 함수
+function ascii(name, i) {
+    const num = name.charCodeAt(i);
+    return (num > 78) ? 91 - num : num - 65;
+}
+
+
+function solution(name) {
+    const arr = [0];
+
+    const answer = [...name]
+        .reduce((answer, s, i) => {
+            if (s === "A") {
+                if (name[i - 1] !== "A") arr.push(continuous(name.substring(i)) - (i - 1));
+                return answer + 1;
+            }
+            return answer + ascii(name, i) + 1;
+        }, 0);
+
+    return answer - Math.max(...arr) - 1;
+}
+
 /*
 문제 설명
 
-조이스틱으로 알파벳 이름을 완성하세요. 
+조이스틱으로 알파벳 이름을 완성하세요.
 맨 처음엔 A로만 이루어져 있습니다.
 
 ex) 완성해야 하는 이름이 세 글자면 AAA, 네 글자면 AAAA
@@ -87,8 +132,8 @@ ex) 완성해야 하는 이름이 세 글자면 AAA, 네 글자면 AAAA
 
 따라서 11번 이동시켜 "JAZ"를 만들 수 있고, 이때가 최소 이동입니다.
 
-만들고자 하는 이름 name이 매개변수로 주어질 때, 
-이름에 대해 조이스틱 조작 횟수의 최솟값을 return 
+만들고자 하는 이름 name이 매개변수로 주어질 때,
+이름에 대해 조이스틱 조작 횟수의 최솟값을 return
 하도록 solution 함수를 만드세요.
 
 제한 사항
